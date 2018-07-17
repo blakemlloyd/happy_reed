86ed487d7db9a7dcd1d601dd0baf375ab40dc139
Blake Lloyd <blakelloyd@live.co.uk>
2018-07-17 20:57:40 +0100

    fix(*): Fix lengths

--- happy_reed.cpp
+++ happy_reed.cpp
@@ -10,16 +10,26 @@ happy_reed::happy_reed(int pin, PubSubClient client)
 
 void happy_reed::setId(char* id)
 {
-    reedConfigTopic = (char*)malloc(strlen(id) + 28 + 16 + 1);
-    snprintf(reedConfigTopic, strlen(id) + 28 + 16, "homeassistant/binary_sensor/%s-reed-%d/config", id, _pin);
+    reedConfigTopic = (char*)malloc(strlen(id) + 28 + 18 + 1);
+    snprintf(reedConfigTopic, strlen(id) + 28 + 18, "homeassistant/binary_sensor/%s-reed-%d/config", id, _pin);
     
-    reedConfig = (char*)malloc(strlen(id) + 10 + 7 + 30 + 1);
-    snprintf(reedConfig, strlen(id) + 10 + 7 + 30, "{\"name\": \"%s-reed-%d\", \"device_class\":\"opening\"}", id, _pin);
+    reedConfig = (char*)malloc(strlen(id) + 10 + 7 + 32 + 1);
+    snprintf(reedConfig, strlen(id) + 10 + 7 + 32, "{\"name\": \"%s-reed-%d\", \"device_class\":\"opening\"}", id, _pin);
     
     reedTopic = (char*)malloc(strlen(id) + 43 + 1);
-    snprintf(reedTopic, strlen(id) + 41, "homeassistant/binary_sensor/%s-reed-%d/state", id, _pin);
+    snprintf(reedTopic, strlen(id) + 43, "homeassistant/binary_sensor/%s-reed-%d/state", id, _pin);
     
     _client.publish(reedConfigTopic, reedConfig, true);
+    
+    int _state = digitalRead(_pin);
+    if(_state == LOW) //closed
+    {
+        _client.publish(reedTopic, "OFF");
+    }
+    else //open
+    {
+        _client.publish(reedTopic, "ON");
+    }
 }
 
 bool happy_reed::checkState()
